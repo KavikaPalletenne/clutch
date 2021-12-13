c56334a0e0d5b9c8364e71351c41a3d488ec4e38
Kavika Palletenne <kbpalletenne@gmail.com>
2021-12-13 14:42:55 +1100

    added components

---
+++ frontend/components/app/GroupTitle.tsx
@@ -0,0 +1,32 @@
+import { FaDiscord } from "react-icons/fa";
+import Link from "next/link";
+import { useState } from 'react';
+
+export default function GroupTitle() {
+    
+    const [groupTitle, setGroupTitle] = useState("MGS Year 11");
+    const [groupDescription, setGroupDescription] = useState("Melbourne Grammar School Yr 11 study group");
+    const [discordLink, setDiscordLink] = useState("https://discord.gg/V38R3ByGQb");
+
+    return(
+        
+        <div className="px-4">
+        <div className="py-4 px-4 shadow-lg inline-block rounded-2xl bg-white hover:shadow-xl duration-150" style={{fontFamily: "Roboto Mono", minWidth: "750px"}}>
+        
+            <div className="inline-block">
+                <h1 className="font-bold text-2xl pb-2 text-exclpurple inline-block">{ groupTitle }</h1>
+                <h1>{ groupDescription }</h1>
+            </div>
+            <Link href={ discordLink }>
+                <a className="float-right inline-block pt-6 flex items-center">
+                    <FaDiscord className="pr-2" size="25px"/>
+                    <h1>
+                        Discord
+                    </h1>
+                </a>
+            </Link>
+
+        </div>
+        </div>
+    )
+}
--- frontend/components/app/members.tsx
+++ frontend/components/app/members.tsx
@@ -1,19 +1,18 @@
+import { useState } from 'react';
+
 export default function Members() {
-    
+
+    const [groupMembers, setGroupMembers] = useState(["@mihir", "@kavika", "@madderz", "@bushby"]);
+    const listMembers = groupMembers.map((d) => <li style={{listStyle: 'none'}} key={d}>{d}</li>);
+
     return(
         
-        <div className="flex justify-center flex-col pt-4 px-4 drop-shadow-xl" style={{fontFamily: "Roboto Mono"}}>
+        <div className="py-4 px-4 shadow-lg inline-block rounded-2xl bg-white hover:shadow-xl duration-150" style={{fontFamily: "Roboto Mono"}}>
         
-            <h1 className="font-bold text-2xl pb-2" style={{background: "linear-gradient(225deg, rgba(140,154,255,1) 0%, rgba(194,144,255,1) 100%)", backgroundClip: "text", WebkitTextFillColor: "transparent"}}>Members</h1>
+            <h1 className="font-bold text-2xl pb-2 text-exclpurple">Members</h1>
             <div>
-                <h1>@mihir</h1>
-                <h1>@kavika</h1>
-                <h1>@snneaky</h1>
-                <h1>@madderz</h1>
-                <h1>@redcadeaux</h1>
-                <h1>@oscrlo</h1>
-            </div>
-            
+                { listMembers }
+            </div>            
         </div>
     )
 }
