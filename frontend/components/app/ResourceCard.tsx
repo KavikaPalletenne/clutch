b01224347a2b639a05c4dd4dd4543e97c337e626
Kavika Palletenne <kbpalletenne@gmail.com>
2022-01-07 22:50:55 +0530

    all group page data now fetched from API

--- frontend/components/app/ResourceCard.tsx
+++ frontend/components/app/ResourceCard.tsx
@@ -1,37 +1,46 @@
 import Link from 'next/link';
 import { useState } from 'react';
+import type { Resource, File, ObjectId } from '../../pages/app/group/[id]';
 
-type Resource = {
-    id: string;
-    title: string;
-    description: string;
-    files: File[];
-}
+// type Resource = {
+//     _id: ObjectId;
+//     user_id: string;
+//     group_id: string;
+//     title: string;
+//     description: string;
+//     tags: string[];
+//     files: File[];
+//     last_edited_at: Date;
+// }
 
-type File = {
-    id: string;
-    title: string;
-    size: string;
-}
+// type ObjectId = {
+//     $oid: string;
+// }
+
+// type File = {
+//     id: string;
+//     title: string;
+//     size: string;
+// }
 
 
 export default function ResourceCard(props: {
     propResource: Resource;
 }) {
 
-    const [resource, setResource] = useState({
-        "id": props.propResource.id,
+    const resource = {
+        "id": props.propResource._id.$oid,
         "title": props.propResource.title,
         "description": props.propResource.description,
         "files": props.propResource.files,
-    });
+    };
     
     
     const listFiles = resource.files.map((f) => 
         <div className="px-4 py-1">
             <Link href={ "https://cdn.examclutch.com/file/" + f.id }>
                 <a>
-                    <div className="py-4 px-4 shadow-sm hover:shadow-md inline-block rounded-2xl bg-white duration-150" style={{fontFamily: "Roboto Mono", }}>
+                    <div className="py-4 px-4 shadow-sm hover:shadow-md inline-block rounded-2xl bg-white duration-150" style={{fontFamily: "Roboto Mono", minWidth: "750px"}}>
                         
                         <li className="justify-center flex" style={{listStyle: 'none'}} key={f.title}>
                             <div className="font-bold">
@@ -49,21 +58,22 @@ export default function ResourceCard(props: {
     );
 
     return(
-        <Link href={ "/app/resource/"+ resource.id }>
-            <a>
-                <div className="px-4 py-2">
-                    <div className="py-4 px-4 shadow-md inline-block rounded-2xl bg-white hover:shadow-lg duration-150" style={{fontFamily: "Roboto Mono"}}>
+            <div className="px-4 py-2">
+                <div className="py-4 px-4 shadow-md inline-block rounded-2xl bg-white hover:shadow-lg duration-150" style={{fontFamily: "Roboto Mono", minWidth: "750px"}}>
 
-                    <div className="inline-block">
-                        <h1 className="font-bold text-2xl pb-2 text-black inline-block">{ resource.title }</h1>
-                        <h1>{ resource.description }</h1>
-                    </div>
-
-                    {listFiles}
-
-                    </div>
+                <div className="inline-block">
+                    <Link href={ "/app/resource/"+ resource.id }>
+                        <a>
+                            <h1 className="font-bold text-2xl pb-2 text-black inline-block hover:text-exclpurple duration-200">{ resource.title }</h1>
+                        </a>
+                    </Link>
+                    <h1>{ resource.description }</h1>
                 </div>
-            </a>
-        </Link>
+
+                {listFiles}
+
+                </div>
+            </div>
+        
     )
 }
---
+++ frontend/pages/app/group/[id].tsx
@@ -0,0 +1,87 @@
+import { useRouter } from "next/router";
+import {SyntheticEvent, useEffect, useState} from  'react';
+
+import Head from "next/head";
+import Members from "../../../components/app/Members";
+import GroupTitle from "../../../components/app/GroupTitle";
+import ResourceCard from "../../../components/app/ResourceCard";
+
+type GroupUser = {
+    id: string;
+    username: string;
+}
+
+type Group = {
+    _id: string;
+    name: string;
+    description: string;
+    discord_link: string;
+    members: GroupUser[];
+    administrators: GroupUser[];
+}
+
+export type Resource = {
+    _id: ObjectId;
+    user_id: string;
+    group_id: string;
+    title: string;
+    description: string;
+    tags: string[];
+    files: File[];
+    last_edited_at: Date;
+}
+
+export type ObjectId = {
+    $oid: string;
+}
+
+export type File = {
+    _id: string;
+    title: String;
+    size: number;
+}
+
+export default function GroupPage({ group, resources }) {
+
+    const listResources = resources.map((r: Resource) =>
+            <ResourceCard propResource={r} />
+    );
+
+    return(
+        <div>
+            <Head>
+                <title>{group.name} - ExamClutch</title>
+                <meta name="description" content="Exam Clutch Dashboard" />
+                <meta name="robots" content="noindex" />
+        <       link rel="icon" href="/favicon.png" />
+            </Head>
+
+            <div className="flex items-start justify-center pt-10">
+                <GroupTitle propGroup={group} />
+                <Members admins={group.administrators} members={group.members} />
+            </div>
+
+            <div className="flex inline-grid justify-center pt-5">
+                { listResources }
+            </div>
+
+        </div>
+    )
+}
+
+
+export async function getServerSideProps(context: any) {
+    
+    const group_res = await fetch(`http://localhost:441/api/group/${context.params.id}`);
+    const group = await group_res.json() as Group;
+
+    const resources_res = await fetch(`http://localhost:440/api/resource/get_all/${context.params.id}`);
+    const resources = await resources_res.json() as Resource[];
+
+    return {
+        props: {
+            group,
+            resources
+        }
+    }
+}
