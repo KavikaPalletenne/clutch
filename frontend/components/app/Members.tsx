307fefbf6d5fe3ae2cf98457d417470eb7585c97
Kavika Palletenne <kbpalletenne@gmail.com>
2022-02-08 20:16:58 +1100

    change shadow change when hovering over

--- frontend/components/app/Members.tsx
+++ frontend/components/app/Members.tsx
@@ -12,7 +12,7 @@ export default function Members(props: {
 
     return(
         
-        <div className="py-4 px-4 shadow-md inline-block rounded-2xl bg-white hover:shadow-lg duration-150" style={{fontFamily: "Roboto Mono"}}>
+        <div className="py-4 px-4 shadow-md inline-block rounded-2xl bg-white duration-150" style={{fontFamily: "Roboto Mono"}}>
         
             <h1 className="font-bold text-2xl pb-2 text-exclpurple">Members</h1>
             <div>
