ee8dc0ede62b3e37e76e1f85d00a50819fe3ff77
Kavika Palletenne <kbpalletenne@gmail.com>
2022-02-15 20:26:30 +1100

    fixed closing tags of links

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,8 +4,8 @@ import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <div>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /> 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap" />
     <Component {...pageProps} />
