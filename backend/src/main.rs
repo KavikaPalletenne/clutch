4ddda754300067c2a56351dbe5289a679084cbcb
Kavika <kbpalletenne@gmail.com>
2022-04-21 13:29:17 +1000

    add tags as filterable attribute to search index

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -61,7 +61,7 @@ async fn main() -> Result<()> {
     // Initialise Meilisearch Connection
     let search_endpoint = env::var("SEARCH_ENDPOINT").expect("Error getting SEARCH_ENDPOINT").to_string();
     let search_index = meilisearch_sdk::client::Client::new(search_endpoint, "masterKey").index("resources");
-    search_index.set_filterable_attributes(["group_id", "subject"]).await.unwrap();
+    search_index.set_filterable_attributes(["group_id", "subject", "tags"]).await.unwrap();
 
     println!("Starting server on port 443.");
     HttpServer::new(move || {
