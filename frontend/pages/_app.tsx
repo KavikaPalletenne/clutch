ab7e54994b6e578d70df0c14c8ee5aafd7ccfd8d
Kavika <kbpalletenne@gmail.com>
2022-04-22 15:49:55 +1000

    add preconnect to fonts

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -8,6 +8,8 @@ function MyApp({ Component, pageProps }: AppProps) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /> 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap" />
+    <link rel="preconnect" href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" />
+    <link rel="preconnect" href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap" />
     <Component {...pageProps} />
   </div>
   )
