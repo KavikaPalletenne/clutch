865aa5982f30ce30310d7c3ebe60862602226dc8
Kavika Palletenne <kbpalletenne@gmail.com>
2022-05-18 22:10:18 +1000

    remove deprecated services for monolith

--- backend/auth-service/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/authservice.proto")?;
-    tonic_build::compile_protos("proto/user/userservice.proto")?;
-    Ok(())
-}
