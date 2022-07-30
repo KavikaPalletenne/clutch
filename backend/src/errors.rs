0996c5bdfe8b7e2bd5a5a25cda1d1d4e0910b04a
Kavika Palletenne <kbpalletenne@gmail.com>
2022-07-30 23:02:22 +1000

    added MyDbError::BadInsert error variant

--- backend/src/errors.rs
+++ backend/src/errors.rs
@@ -3,9 +3,11 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum MyDbError {
     #[error("No such row with id \"{id:?}\"")]
-    NoSuchRow { id: String },
+    NoSuchRow { id: String }, // Need to add table name to this
     #[error("Row with id \"{id:?}\" in table \"{table_name:?}\" did not update properly")]
     BadUpdate { id: String, table_name: String },
+    #[error("Row in table \"{table_name:?}\" was not inserted row properly")]
+    BadInsert { table_name: String },
 }
 
 // #[derive(Error, Debug)]
