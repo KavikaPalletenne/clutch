35211993ddad81145af42a3f4d3e05e222da3916
Kavika Palletenne <kbpalletenne@gmail.com>
2022-07-31 21:22:34 +1000

    add redirect to ezoic ads.txt in next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/ads.txt',
+        destination: 'http://srv.adstxtmanager.com/19390/examcldutch.com',
+        permanent: true,
+      },
+    ]
+  }
 }
