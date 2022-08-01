f37ef526369a3513db12e6a2bb2becf1cb93a06f
Kavika <kbpalletenne@gmail.com>
2022-08-01 10:26:37 +1000

    add ads.txt route to main branch

---
+++ frontend/pages/ads.txt.tsx
@@ -0,0 +1,16 @@
+import { GetServerSideProps } from "next";
+
+export default function AdsTxtPage() {
+  return(
+    <div></div>
+  )
+}
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
+
+  return {
+    redirect: {
+        destination: 'https://srv.adstxtmanager.com/19390/examclutch.com',
+        permanent: false,
+    }
+}
+}
